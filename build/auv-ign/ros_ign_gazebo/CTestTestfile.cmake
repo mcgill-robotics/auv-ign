3c58ad744b4e1d5e1fbed4f3d362c04f45146ae9
antoinedang <antoine.dangeard@gmail.com>
2023-07-22 00:04:25 -0400

    fix mistake during build process

--- build/auv-ign/ros_ign_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson/AUV-2023/ign_catkin_ws/src/auv-ign/ros_ign_gazebo
-# Build directory: /home/jetson/AUV-2023/ign_catkin_ws/build/auv-ign/ros_ign_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
